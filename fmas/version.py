196bb8c6f38aa150de04b7978e90fc21cfdfc561
omelchert <melchert@iqo.uni-hannover.de>
2021-04-23 11:04:12 +0200

    updated version identifier and wheel

--- fmas/version.py
+++ fmas/version.py
@@ -3,16 +3,20 @@ py-fmas version information.
 
 .. moduleauthor: Oliver Melchert <melchert@iqo.uni-hannover.de>
 """
-__version__ = "1.3.2"
+__version__ = "1.0"
 
 """
 CHANGELOG:
 
-1.3.2 (So 18 Apr 2021 11:42:51 CEST)
+1.0 (Fr 23 Apr 2021 10:57:15 CEST)
+----------------------------------
+* increased major version number prior to submission -- OM
+
+0.3.2 (So 18 Apr 2021 11:42:51 CEST)
 ------------------------------------
 * added root-power loss to ModelBaseClass and derived fmas models --  OM
 
-1.3.1 (Mo 12 Apr 2021 09:31:30 CEST)
+0.3.1 (Mo 12 Apr 2021 09:31:30 CEST)
 ------------------------------------
 * added CQE_RK$IP to py-fmas solver module - OM
 
